208e03cfe77bfbf523b485d889c563a6bccd511b
kurniayazid <yazidega@gmail.com>
2022-11-01 18:55:50 -0400

    Add test for single letter text

--- tests/test_cipher_ey2335.py
+++ tests/test_cipher_ey2335.py
@@ -1,1 +1,12 @@
 from cipher_ey2335 import cipher_ey2335
+
+# Importing pytest
+import pytest
+
+## 1A - Testing single letter word
+def test_singleword_cipher():
+    singleword = ['a','b','c'] # Generate set of single word to be tested
+    expected = ['c','d','e'] # Generate expected return
+    # Iterate a set of test for every member in `singleworld``
+    for i in range(0,3):
+        assert cipher_ey2335.cipher(singleword[i], shift=2) == expected[i]
